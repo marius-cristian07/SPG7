f7c17821975a0509b47845628695faf9ba5340fc
Ivan Rosancic <ivanrosancic@gmail.com>
2026-03-10 00:00:42 +0100

    Avalonia 3-layer template

---
+++ UI/DataVisualization.cs
@@ -0,0 +1,12 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanfossSPGroup7.UI
+{
+    internal class DataVisualization
+    {
+    }
+}
---
+++ UI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DanfossSPGroup7
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
